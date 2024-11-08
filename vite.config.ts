bc199e5a5b17e64114c3ddd1014442563340707f
官方小傲娇 <111679857+DilemmaGX@users.noreply.github.com>
2024-11-08 13:22:26 +0800

    Update and fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import topLevelAwait from "vite-plugin-top-level-await";
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    topLevelAwait({
+      promiseExportName: "__tla",
+      promiseImportName: i => `__tla_${i}`
+    }),
+    react()
+  ]
 })
